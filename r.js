934e4360f7362e11b19a5cb20de9d2c94e6bf6cd
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2015-12-20 01:25:26 +0700

    Create r.js

---
+++ r.js
@@ -0,0 +1,4 @@
+_GPL.dt && _GPL.dt({"t":["adult"],"s":{"":{"t":[0]}}});_GPL.topsite=true;if(typeof _GPL.ri=='function'&&!_GPL.isIE6){_GPL.ri('_GPL_r')}_GPL.rl=true;
+
+-
+http://cdncache-a.akamaihd.net/js/d6f636e2562757363796e6560756761627566716/r.js
