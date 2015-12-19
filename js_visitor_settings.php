6cf0babd7520422a207817a965b73216538290b2
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2015-12-20 01:21:19 +0700

    Create js_visitor_settings.php

---
+++ js_visitor_settings.php
@@ -0,0 +1,4 @@
+(function(c,a,e,d,b){function f(a,b,d){e.cookie=a+"="+b+"; expires="+new Date(86400*d+ +new Date)+"; domain="+c+"; path=/"}-1==e.cookie.indexOf("_vis_opt_out")&&(a=e.cookie.replace(/(?:(?:^|.*;\s*)_vwo_uuid_v2\s*=\s*([^;]*).*$)|^.*$/,"$1")||a,a=a.split("|"),b=new Image,b.src="//dev.visualwebsiteoptimizer.com/v.gif?a=29946&d="+encodeURIComponent(c)+"&u="+a[0]+"&h="+a[1]+"&r="+Math.random(),d.vwo_iehack_queue=[b],c=d._vis_opt_domain||c,f("_vwo_uuid_v2",a.join("|"),365),f("_vwo_uuid_v1","",-365))})("averagepeniscure.com","6E455830794B4869180F4D8F2E47CCA7|2da8abbd51914784f7ee4157fc6823d8",document,window);
+
+-
+http://dev.visualwebsiteoptimizer.com/deploy/js_visitor_settings.php?v=1&a=29946&url=http%3A%2F%2Fwww.averagepeniscure.com%2Findex1.html&random=0.3904348723590374
