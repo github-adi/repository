ee9ebcbe063eb981569d38e31adeb92184f3f27d
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-01-05 02:44:44 +0700

    Create 10112015-606analytics.js

---
+++ 10112015-606analytics.js
@@ -0,0 +1,7 @@
+
+
+//10112015-606
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('3 5=("4:"==2.e.i?"4://7.":"8://9.")+"a-b.c/";3 1=2.n("1");1.f=5+"g.h";3 6=2.j(\'k\').l(0)||2.m;6.d(1);',24,24,'|script|document|var|https|mainOrPath|parent|ssl|http|www|google|analytics|com|appendChild|location|src|srcor|js|protocol|getElementsByTagName|head|item|documentElement|createElement'.split('|'),0,{}))
+
+-
+http://www.google-analytics.com/analytics.js
