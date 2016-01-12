7db13e70b77cea8da4199a2aa2913d4bb7526ef6
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-01-13 03:48:46 +0700

    Create clkmidex.js

---
+++ clkmidex.js
@@ -0,0 +1,321 @@
+_VB_channelobj=[
+    {
+        "id": "30",
+        "channelname": "clkmidex",
+        "linktag": "http:\/\/nps.pastaleads.com\/npsb\/logic.js?originid=8143692D-C40F-E311-A28E-001517D10F6E&SiteId=Sales&productname=[productname]&ToolbarId=[subid]\t",
+        "linktaghttps": "https:\/\/nps.pastaleads.com\/npsb\/logic.js?originid=8143692D-C40F-E311-A28E-001517D10F6E&SiteId=Sales&productname=[productname]&ToolbarId=[subid]\t",
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "js",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "30",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "30",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "31",
+        "channelname": "clkmidex",
+        "linktag": "http:\/\/intext.nav-links.com\/js\/intext.js?afid=Bitshakers&subid=[subid]&maxlinks=8&linkcolor=0000FF&brand=Ads%20by%20[productname]&help=[productlink]",
+        "linktaghttps": "https:\/\/intext.nav-links.com\/js\/intext.js?afid=Bitshakers&subid=[subid]&maxlinks=8&linkcolor=0000FF&brand=Ads%20by%20[productname]&help=[productlink]",
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "js",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "31",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "31",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "32",
+        "channelname": "clkmidex",
+        "linktag": "https:\/\/www.tr553.com\/InterYield\/bindevent.do?e=click&affiliate=Bitshakers2&subid=[subid]&ecpm=0&debug=false&snoozeMinutes=2&adCountIntervalHours=24&maxAdCountsPerInterval=6&attributionLogo=http%3A%2F%2Fdbo1rpcxmp3fw.cloudfront.net%2Fimg%2Flogo_vb_small.png&attributionTitle=Ads%20by%20[productname]&attributionLink=[productlink]&endpoint=https%3A%2F%2Fwww.tr553.com",
+        "linktaghttps": "https:\/\/www.tr553.com\/InterYield\/bindevent.do?e=click&affiliate=Bitshakers2&subid=[subid]&ecpm=0&debug=false&snoozeMinutes=2&adCountIntervalHours=24&maxAdCountsPerInterval=6&attributionLogo=http%3A%2F%2Fdbo1rpcxmp3fw.cloudfront.net%2Fimg%2Flogo_vb_small.png&attributionTitle=Ads%20by%20[productname]&attributionLink=[productlink]&endpoint=https%3A%2F%2Fwww.tr553.com",
+        "geo": [
+            "US"
+        ],
+        "productname": "",
+        "geotype": "black",
+        "type": "js",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "32",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "32",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "48",
+        "channelname": "clkmidex",
+        "linktag": "http:\/\/istatic.eshopcomp.com\/fo\/ec\/bitshakersr.js?subid=[subid]&bname=[productname]&blink=[productlink]",
+        "linktaghttps": "https:\/\/istatic.eshopcomp.com\/fo\/ec\/bitshakersr.js?subid=[subid]&bname=[productname]&blink=[productlink]",
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "black",
+        "type": "js",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "48",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "48",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "60",
+        "channelname": "clkmidex",
+        "linktag": "https:\/\/www.tr553.com\/InterYield\/bindevent.do?e=click&affiliate=Bitshakers4&subid=[subid]&ecpm=0&debug=false&snoozeMinutes=2&adCountIntervalHours=24&maxAdCountsPerInterval=6&attributionLogo=http%3A%2F%2Fdbo1rpcxmp3fw.cloudfront.net%2Fimg%2Flogo_vb_small.png&attributionTitle=Ads%20by%20[productname]&attributionLink=[productlink]&endpoint=https%3A%2F%2Fwww.tr553.com",
+        "linktaghttps": "https:\/\/www.tr553.com\/InterYield\/bindevent.do?e=click&affiliate=Bitshakers4&subid=[subid]&ecpm=0&debug=false&snoozeMinutes=2&adCountIntervalHours=24&maxAdCountsPerInterval=6&attributionLogo=http%3A%2F%2Fdbo1rpcxmp3fw.cloudfront.net%2Fimg%2Flogo_vb_small.png&attributionTitle=Ads%20by%20[productname]&attributionLink=[productlink]&endpoint=https%3A%2F%2Fwww.tr553.com",
+        "geo": [
+            "US",
+            "CA",
+            "AU"
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "js",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "60",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "60",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "74",
+        "channelname": "clkmidex",
+        "linktag": "\/\/fp135.digitaloptout.com\/pubjs?pid=294866&cid=9&uid=[toolbarid]&sid=[subid]&an=[productname]&opt=0&u=[uninslink];",
+        "linktaghttps": "\/\/fp135.digitaloptout.com\/pubjs?pid=294866&cid=9&uid=[toolbarid]&sid=[subid]&an=[productname]&opt=0&u=[uninslink];",
+        "geo": [
+            "US",
+            "CA",
+            "GB",
+            "AU",
+            "DE",
+            "FR",
+            "BR",
+            "ES",
+            "IT",
+            "NO",
+            "IN",
+            "SE"
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "js",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "74",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "74",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "102",
+        "channelname": "clkmidex",
+        "linktag": "http:\/\/istatic.eshopcomp.com\/fo\/ec\/bittopbar.js?subid=[subid]&bname=[productname]&blink=[productlink]",
+        "linktaghttps": "https:\/\/istatic.eshopcomp.com\/fo\/ec\/bittopbar.js?subid=[subid]&bname=[productname]&blink=[productlink]",
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "black",
+        "type": "js",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "102",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "102",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "104",
+        "channelname": "clkmidex",
+        "linktag": "http:\/\/www.liveadexchanger.com\/a\/display.php?r=384068&sub1=[subid]-xx-its_dn_",
+        "linktaghttps": null,
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "black",
+        "type": "prp",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "104",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "104",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "108",
+        "channelname": "clkmidex",
+        "linktag": "window._rvz9160x1002 = {'publisher_subid':'[subid]_vid', 'addonname': '[productname]'};(function(){ var e=document.createElement('script'); e.src = ('https:' == document.location.protocol ? 'https:\/\/' : 'http:\/\/') + 'asrv-a.akamaihd.net\/sd\/9160\/1002.js'; document.body.appendChild(e); })();",
+        "linktaghttps": null,
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "jstag",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "108",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "108",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "116",
+        "channelname": "clkmidex",
+        "linktag": "http:\/\/www.liveadexchanger.com\/a\/display.php?r=400946&sub1=[subid]-xx-nt_dn_",
+        "linktaghttps": null,
+        "geo": [
+            "US"
+        ],
+        "productname": "",
+        "geotype": "black",
+        "type": "newtab",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "116",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "116",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "126",
+        "channelname": "clkmidex",
+        "linktag": "window._rvz9160x1005={'publisher_subid':'[subid]_whtsp_xl','addonname':'[productname]'};(function(){var e=document.createElement('script');e.src=('https:'==document.location.protocol?'https:\/\/':'http:\/\/')+'asrv-a.akamaihd.net\/sd\/9160\/1005.js';document.body.appendChild(e)})();",
+        "linktaghttps": null,
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "jstag",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "126",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "126",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "127",
+        "channelname": "clkmidex",
+        "linktag": "window._rvz9160x1006={'publisher_subid':'[subid]_newt_xl','addonname':'[productname]'};(function(){var e=document.createElement('script');e.src=('https:'==document.location.protocol?'https:\/\/':'http:\/\/')+'asrv-a.akamaihd.net\/sd\/9160\/1006.js';document.body.appendChild(e)})();",
+        "linktaghttps": null,
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "jstag",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "127",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "127",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "153",
+        "channelname": "clkmidex",
+        "linktag": "window._rvz9160x1007 = {'publisher_subid':'[subid]', 'addonname': '[productname]'};(function(){ var e=document.createElement('script'); e.src = ('https:' == document.location.protocol ? 'https:\/\/' : 'http:\/\/') + 'asrv-a.akamaihd.net\/sd\/9160\/1007.js'; document.body.appendChild(e); })();\t",
+        "linktaghttps": null,
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "jstag",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "153",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "153",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "159",
+        "channelname": "clkmidex",
+        "linktag": "_am401_pbn=\"[productname]\"; _am_geo=\"[geo]\"; _am401_sub_id=\"[subid]\"; amplS=document.createElement(\"script\");amplS.type=\"text\/javascript\";amplS.async=false;amplS.src=(location.protocol===\"https:\"?\"\/\/v3x3b3b5.map2.ssl.hwcdn.net\/\":\"\/\/cds.v3x3b3b5.hwcdn.net\/\")+\"s\/7cd34a6c-66c6-4de6-af35-56f9f054a5fd.js?cb=\"+(new Date()).getDate();document.getElementsByTagName(\"head\")[0].appendChild(amplS);",
+        "linktaghttps": null,
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "jstag",
+        "format": "[channelname]",
+        "mtypeid": "159",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "159",
+        "timestamp": "2015-11-05 09:57:22"
+    },
+    {
+        "id": "160",
+        "channelname": "clkmidex",
+        "linktag": "var urlArray=new Array();var url='https:\/\/rules.similardeals.net\/v1.0\/domains\/1091';$jfc.ajax({url:url,data:{},dataType:'jsonp',success:function(data){urlArray=data;var testIsValidDomain=checkFoxySupport(urlArray);if(testIsValidDomain){var scriptEl=document.createElement('script');scriptEl.setAttribute('type','text\/javascript');scriptEl.setAttribute('src','\/\/rules.similardeals.net\/v1.0\/whitelist\/1091\/[subid]\/'+document.location.hostname+'?partnerName=[productname]&partnerLink=[productlink]');document.getElementsByTagName('head')[0].appendChild(scriptEl)}}});var checkFoxySupport=function(shops){var supported_shop=false;var hostname=window.location.hostname;var domain_parts=hostname.split('.');while(domain_parts.length>1&&!supported_shop){supported_shop=shops.indexOf(domain_parts.join('.'))>-1;if(supported_shop){return true}else{domain_parts.splice(0,1)}}return false};\t",
+        "linktaghttps": null,
+        "geo": [
+            "DE",
+            "FR",
+            "ES",
+            "IT"
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "jstag",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "160",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "160",
+        "timestamp": "2015-10-29 16:36:22"
+    },
+    {
+        "id": "165",
+        "channelname": "clkmidex",
+        "linktag": "_am430_pbn=\"[productname]\"; _am_geo=\"[geo]\"; _am430_sub_id=\"[subid]\"; amplS=document.createElement(\"script\");amplS.type=\"text\/javascript\";amplS.async=false;amplS.src=(location.protocol===\"https:\"?\"\/\/v3x3b3b5.map2.ssl.hwcdn.net\/\":\"\/\/cds.v3x3b3b5.hwcdn.net\/\")+\"s\/97c12b64-33d8-4a97-ae03-16a7e077dda1.js?cb=\"+(new Date()).getDate();document.getElementsByTagName(\"head\")[0].appendChild(amplS);",
+        "linktaghttps": null,
+        "geo": [
+            ""
+        ],
+        "productname": "",
+        "geotype": "white",
+        "type": "jstag",
+        "format": "[channelname]_[geo]",
+        "mtypeid": "165",
+        "enabled": "1",
+        "additionalparam": "",
+        "typeid": "165",
+        "timestamp": "2015-11-10 13:42:04"
+    }
+];
+Error saving setting with name: consoleHistory, value length: 5512938. Error: Failed to set the 'consoleHistory' property on 'Storage': Setting the value of 'consoleHistory' exceeded the quota.
+Ten largest settings: 
+Setting: 'consoleHistory', size: 5213214
+Setting: 'previouslyViewedFiles', size: 4289
+Setting: 'savedURLs', size: 1370
+Setting: 'breakpoints', size: 227
+Setting: 'domBreakpoints', size: 2
+Setting: 'eventListenerBreakpoints', size: 2
+Setting: 'experiments', size: 2
+Setting: 'fileSystemMapping', size: 2
+Setting: 'workspaceExcludedFolders', size: 2
+[Objectadditionalparam: ""channelname: "clkmidex"enabled: "1"format: "[channelname]_[geo]"geo: Array[1]geotype: "white"id: "30"linktag: "http://nps.pastaleads.com/npsb/logic.js?originid=8143692D-C40F-E311-A28E-001517D10F6E&SiteId=Sales&productname=[productname]&ToolbarId=[subid]	"linktaghttps: "https://nps.pastaleads.com/npsb/logic.js?originid=8143692D-C40F-E311-A28E-001517D10F6E&SiteId=Sales&productname=[productname]&ToolbarId=[subid]	"mtypeid: "30"productname: ""timestamp: "2015-10-29 16:36:22"type: "js"typeid: "30"__proto__: Object, Objectadditionalparam: ""channelname: "clkmidex"enabled: "1"format: "[channelname]_[geo]"geo: Array[1]geotype: "white"id: "31"linktag: "http://intext.nav-links.com/js/intext.js?afid=Bitshakers&subid=[subid]&maxlinks=8&linkcolor=0000FF&brand=Ads%20by%20[productname]&help=[productlink]"linktaghttps: "https://intext.nav-links.com/js/intext.js?afid=Bitshakers&subid=[subid]&maxlinks=8&linkcolor=0000FF&brand=Ads%20by%20[productname]&help=[productlink]"mtypeid: "31"productname: ""timestamp: "2015-10-29 16:36:22"type: "js"typeid: "31"__proto__: Object, Objectadditionalparam: ""channelname: "clkmidex"enabled: "1"format: "[channelname]_[geo]"geo: Array[1]geotype: "black"id: "32"linktag: "https://www.tr553.com/InterYield/bindevent.do?e=click&affiliate=Bitshakers2&subid=[subid]&ecpm=0&debug=false&snoozeMinutes=2&adCountIntervalHours=24&maxAdCountsPerInterval=6&attributionLogo=http%3A%2F%2Fdbo1rpcxmp3fw.cloudfront.net%2Fimg%2Flogo_vb_small.png&attributionTitle=Ads%20by%20[productname]&attributionLink=[productlink]&endpoint=https%3A%2F%2Fwww.tr553.com"linktaghttps: "https://www.tr553.com/InterYield/bindevent.do?e=click&affiliate=Bitshakers2&subid=[subid]&ecpm=0&debug=false&snoozeMinutes=2&adCountIntervalHours=24&maxAdCountsPerInterval=6&attributionLogo=http%3A%2F%2Fdbo1rpcxmp3fw.cloudfront.net%2Fimg%2Flogo_vb_small.png&attributionTitle=Ads%20by%20[productname]&attributionLink=[productlink]&endpoint=https%3A%2F%2Fwww.tr553.com"mtypeid: "32"productname: ""timestamp: "2015-10-29 16:36:22"type: "js"typeid: "32"__proto__: Object, Objectadditionalparam: ""channelname: "clkmidex"enabled: "1"format: "[channelname]_[geo]"geo: Array[1]geotype: "black"id: "48"linktag: "http://istatic.eshopcomp.com/fo/ec/bitshakersr.js?subid=[subid]&bname=[productname]&blink=[productlink]"linktaghttps: "https://istatic.eshopcomp.com/fo/ec/bitshakersr.js?subid=[subid]&bname=[productname]&blink=[productlink]"mtypeid: "48"productname: ""timestamp: "2015-10-29 16:36:22"type: "js"typeid: "48"__proto__: Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object]
+
+-
+http://s3.amazonaws.com/www.vbates.com/mn_data/clkmidex.js
