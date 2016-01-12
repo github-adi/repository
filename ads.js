b99284934bbe5144136345d80be78c524d01a52a
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-01-13 03:04:52 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,17 @@
+var prechek_adblck = true;
+
+Error saving setting with name: consoleHistory, value length: 5435082. Error: Failed to set the 'consoleHistory' property on 'Storage': Setting the value of 'consoleHistory' exceeded the quota.
+Ten largest settings: 
+Setting: 'consoleHistory', size: 5213214
+Setting: 'previouslyViewedFiles', size: 4289
+Setting: 'savedURLs', size: 1150
+Setting: 'breakpoints', size: 227
+Setting: 'domBreakpoints', size: 2
+Setting: 'eventListenerBreakpoints', size: 2
+Setting: 'experiments', size: 2
+Setting: 'fileSystemMapping', size: 2
+Setting: 'workspaceExcludedFolders', size: 2
+undefined
+
+-
+http://q2u3z6t7.ssl.hwcdn.net/js/ads.js
