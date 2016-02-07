057ce878523f6a7bdf7c64c1867d0c605ef1c7c7
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-02-07 20:26:47 +0700

    Create r.js

---
+++ Sorces--http/cdncache-a.akamaihd.net/js/d6f636e216471646070716/r.js
@@ -0,0 +1,30 @@
+-Sorces:
+-http://cdncache-a.akamaihd.net/js/d6f636e216471646070716/r.js:
+-Evaluate in consosle:
+_GPL.dt && _GPL.dt({"t":["apn_whitelist"],"s":{"":{"t":[0]}}});if(typeof _GPL.ri=='function'&&!_GPL.isIE6){_GPL.ri('_GPL_r')}_GPL.rl=true;
+
+Error saving setting with name: consoleHistory, value length: 5235016. Error: Failed to set the 'consoleHistory' property on 'Storage': Setting the value of 'consoleHistory' exceeded the quota.
+Ten largest settings: 
+Setting: 'consoleHistory', size: 5234863
+Setting: 'previouslyViewedFiles', size: 4060
+Setting: 'savedURLs', size: 3473
+Setting: 'breakpoints', size: 227
+Setting: 'watchExpressions', size: 13
+Setting: 'lastSelectedSourcesSidebarPaneTab', size: 7
+Setting: 'domBreakpoints', size: 2
+Setting: 'eventListenerBreakpoints', size: 2
+Setting: 'experiments', size: 2
+Setting: 'workspaceExcludedFolders', size: 2
+true
+Error saving setting with name: savedURLs, value length: 3514. Error: Failed to set the 'savedURLs' property on 'Storage': Setting the value of 'savedURLs' exceeded the quota.
+Ten largest settings: 
+Setting: 'consoleHistory', size: 5234863
+Setting: 'previouslyViewedFiles', size: 4060
+Setting: 'savedURLs', size: 3473
+Setting: 'breakpoints', size: 227
+Setting: 'watchExpressions', size: 13
+Setting: 'lastSelectedSourcesSidebarPaneTab', size: 7
+Setting: 'domBreakpoints', size: 2
+Setting: 'eventListenerBreakpoints', size: 2
+Setting: 'experiments', size: 2
+Setting: 'workspaceExcludedFolders', size: 2
