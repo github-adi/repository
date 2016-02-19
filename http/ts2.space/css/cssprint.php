c42c446a28abb5a6a634ca221ef3fe36ee16ccac
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-02-20 03:13:42 +0700

    Create cssprint.php

---
+++ http/ts2.space/css/cssprint.php
@@ -0,0 +1,15 @@
+-Link Address:
+http://ts2.space/css/cssprint.php
+
+@CHARSET "UTF-8";body { background: #FFFFFF; margin: 0; padding: 10px; color: #000000; }
+#header { display: none; }
+#mainMenu { display: none; }
+#contentBg { display: none; }
+#columnLeft-s { display: none; }
+#columnRight-s { width: 100%; font-size: 12px; }
+#siteMenu { display: none; }
+#footer { display: none; }
+#copyright { display: none; }
+
+a { color: #000000; }
+a:hover { color: #000000; }
