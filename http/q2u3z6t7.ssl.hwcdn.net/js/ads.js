656e9195e83e7cae8fc54646d9418e94815476e2
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-01 00:09:36 +0700

    Create ads.js

---
+++ http/q2u3z6t7.ssl.hwcdn.net/js/ads.js
@@ -0,0 +1,30 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+-This is script not <script src/> tag:
+http://q2u3z6t7.ssl.hwcdn.net/js/ads.js
+But function identifier.
+-To be evaluated in console:
+fbMyApp.html
+ads.js
+2 requests ❘ 322 B transferred ❘ Finish: 658 ms ❘ DOMContentLoaded: 837 ms ❘ Load: 834 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+var prechek_adblck = true;
+2016-03-01 00:04:06.832 Error saving setting with name: consoleHistory, value length: 5235177. Error: Failed to set the 'consoleHistory' property on 'Storage': Setting the value of 'consoleHistory' exceeded the quota.
+2016-03-01 00:04:06.848 Ten largest settings: 
+2016-03-01 00:04:07.058 Setting: 'consoleHistory', size: 5235146
+2016-03-01 00:04:07.059 Setting: 'savedURLs', size: 3672
+2016-03-01 00:04:07.059 Setting: 'previouslyViewedFiles', size: 3554
+2016-03-01 00:04:07.060 Setting: 'breakpoints', size: 227
+2016-03-01 00:04:07.060 Setting: 'watchExpressions', size: 45
+2016-03-01 00:04:07.060 Setting: 'lastSelectedSourcesSidebarPaneTab', size: 7
+2016-03-01 00:04:07.060 Setting: 'domBreakpoints', size: 2
+.
+
