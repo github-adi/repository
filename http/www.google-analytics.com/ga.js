3317eb2e68e2672ccdc5990011e64d0016753e22
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-07 00:08:57 +0700

    Create ga.js

---
+++ http/www.google-analytics.com/ga.js
@@ -0,0 +1,39 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+http://www.google-analytics.com/ga.js
+-To be evaluated in console:
+tpc.html	GET	Finished	file	file			document	Other			0 B	26 ms	Highest	0									
+ga.js	GET	304	http/1.1	http	www.google-analytics.com	107.178.255.88:80	script	tpc.html:18			454 B	222 ms	Medium	13685					"97709cdd367967e9e07a6a7a13353ac5"		nginx/1.8.1		
+UsersAxiooFavoriteswallet.js');	GET	(failed)		file			document	tpc.html:13			0 B	425 ms	Highest	0									
+srcor.js	GET	304	http/1.1	http	www.google-analytics.com	107.178.255.88:80	script	Other			454 B	343 ms	Low	13685					"4abe1bfac0d2c0a8819e9b47396d91de"		nginx/1.8.1		
+data:image/png;base…	GET	200	data	data			png	data:text/html,chro…:3943			(from cache)	6 ms	Lowest	0									
+119 requests ❘ 6.7 MB transferred ❘ Finish: 1.4 min ❘ DOMContentLoaded: 1.69 s ❘ Load: 51.30 s
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-07 00:04:08.550
+bitshakersr.js?subid=gen_&bname=Capricornus&blink=http://canopus.orgfree.com/uninstall.html:46 GET file://pstatic.bestpriceninja.com/nwp/v0_0_1004/release/Shared/App/SharedApp.js?t=3 net::ERR_FILE_NOT_FOUND
+2016-03-07 00:04:11.270
+main.js:23 POST https://mybrowserpages.com/json/index.php?name=EasySearch net::ERR_CONNECTION_TIMED_OUT
+2016-03-07 00:04:11.270
+main.js:23 XHR failed loading: POST "https://mybrowserpages.com/json/index.php?name=EasySearch".
+2016-03-07 00:04:11.273
+VM942:1 Uncaught SyntaxError: Unexpected end of input
+2016-03-07 00:04:12.174
+main.js:23 POST https://mybrowserpages.com/json/index.php?name=EasySearch net::ERR_CONNECTION_TIMED_OUT
+2016-03-07 00:04:12.174
+main.js:23 XHR failed loading: POST "https://mybrowserpages.com/json/index.php?name=EasySearch".
+2016-03-07 00:04:12.177
+VM943:1 Uncaught SyntaxError: Unexpected end of input
+2016-03-07 00:04:16.333 like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FSexy-Three-Kingdoms-Community%2F712892212113…:3 POST https://mybrowserpages.com/json/index.php?name=EasySearch net::ERR_CONNECTION_TIMED_OUT
+2016-03-07 00:04:16.334
+main.js:23 XHR failed loading: POST "https://mybrowserpages.com/json/index.php?name=EasySearch".
+2016-03-07 00:04:16.336
+VM944:1 Uncaught SyntaxError: Unexpected end of input
+.
