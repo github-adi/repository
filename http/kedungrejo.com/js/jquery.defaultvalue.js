4f974e94b60b2b0d8bcc0f3d3a2a926b31fce272
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-08 01:14:26 +0700

    Create jquery.defaultvalue.js

---
+++ http/kedungrejo.com/js/jquery.defaultvalue.js
@@ -0,0 +1,20 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+http://kedungrejo.com/js/jquery.defaultvalue.js
+-To be evaluated in console:
+fbMyApp.html	GET	Finished	file	file			document	Other			0 B	12 ms	Highest	0									
+jquery.defaultvalue.js	GET	304	http/1.1	http	kedungrejo.com	103.28.22.108:80	script	fbMyApp.html:1	6		352 B	542 ms	Medium	30017		Keep-Alive			"3980da-6fe-50141c01a3bc0"		Apache/2.2.24 (Unix) mod_ssl/2.2.24 OpenSSL/0.9.8e-fips-rhel5 mod_auth_passthrough/2.1 mod_bwlimited/1.4		
+2 requests ❘ 352 B transferred ❘ Finish: 575 ms ❘ DOMContentLoaded: 619 ms ❘ Load: 618 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-08 01:12:01.419
+jquery.defaultvalue.js:60 Uncaught ReferenceError: jQuery is not defined
+(anonymous function)	@	jquery.defaultvalue.js:60
+.
