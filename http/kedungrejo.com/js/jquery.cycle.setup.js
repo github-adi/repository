d48ef1008c0f7d3a8b236474eab8a5c138483c9c
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-08 00:52:07 +0700

    Create jquery.cycle.setup.js

---
+++ http/kedungrejo.com/js/jquery.cycle.setup.js
@@ -0,0 +1,20 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+http://kedungrejo.com/js/jquery.cycle.setup.js
+-To be evaluated in console:
+tpc.html	GET	Finished	file	file			document	Other			0 B	5 ms	Highest	0									
+jquery.cycle.setup.js	GET	304	http/1.1	http	kedungrejo.com	103.28.22.108:80	script	tpc.html:19	6		352 B	357 ms	Medium	27173		Keep-Alive			"3980d9-203-50141c00af980"		Apache/2.2.24 (Unix) mod_ssl/2.2.24 OpenSSL/0.9.8e-fips-rhel5 mod_auth_passthrough/2.1 mod_bwlimited/1.4		
+2 requests ❘ 352 B transferred ❘ Finish: 411 ms ❘ DOMContentLoaded: 509 ms ❘ Load: 508 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-08 00:46:57.230
+jquery.cycle.setup.js:1 Uncaught ReferenceError: $ is not defined
+(anonymous function)	@	jquery.cycle.setup.js:1
+.
