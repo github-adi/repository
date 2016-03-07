8ebab633756a4ff5685260bca9a23ad8ff4102ac
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-08 02:23:10 +0700

    Create jquery.scrollTo-min.js

---
+++ http/kedungrejo.com/js/jquery.scrollTo-min.js
@@ -0,0 +1,29 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+http://kedungrejo.com/js/jquery.scrollTo-min.js
+-To be evaluated in console:
+tpc.html	GET	Finished	file	file			document	Other			0 B	10 ms	Highest	0									
+jquery.scrollTo-min.js	GET	304	http/1.1	http	kedungrejo.com	103.28.22.108:80	script	tpc.html:19	6		373 B	245 ms	Medium	51838		Keep-Alive			"3980db-8d6-50141c0297e00"		Apache/2.2.24 (Unix) mod_ssl/2.2.24 OpenSSL/0.9.8e-fips-rhel5 mod_auth_passthrough/2.1 mod_bwlimited/1.4 FrontPage/5.0.2.2635		
+2 requests ❘ 373 B transferred ❘ Finish: 399 ms ❘ DOMContentLoaded: 452 ms ❘ Load: 448 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-08 02:20:37.422
+jquery.scrollTo-min.js:11 Uncaught ReferenceError: jQuery is not defined
+(anonymous function)	@	jquery.scrollTo-min.js:11
+2016-03-08 02:20:50.164 Error saving setting with name: previouslyViewedFiles, value length: 3608. Error: Failed to set the 'previouslyViewedFiles' property on 'Storage': Setting the value of 'previouslyViewedFiles' exceeded the quota.
+2016-03-08 02:20:50.167 Ten largest settings: 
+2016-03-08 02:20:50.359 Setting: 'consoleHistory', size: 5235172
+2016-03-08 02:20:50.359 Setting: 'savedURLs', size: 3672
+2016-03-08 02:20:50.360 Setting: 'previouslyViewedFiles', size: 3533
+2016-03-08 02:20:50.360 Setting: 'breakpoints', size: 227
+2016-03-08 02:20:50.360 Setting: 'watchExpressions', size: 45
+2016-03-08 02:20:50.360 Setting: 'lastSelectedSourcesSidebarPaneTab', size: 7
+2016-03-08 02:20:50.360 Setting: 'domBreakpoints', size: 2
+.
