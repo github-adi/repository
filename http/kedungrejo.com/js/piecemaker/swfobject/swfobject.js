5f24d4e73f0ecc53ee3437184771d84acaa0bace
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-09 03:20:03 +0700

    Create swfobject.js

---
+++ http/kedungrejo.com/js/piecemaker/swfobject/swfobject.js
@@ -0,0 +1,45 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+http://kedungrejo.com/js/piecemaker/swfobject/swfobject.js
+-To be evaluated in console:
+Request URL:http://kedungrejo.com/js/piecemaker/swfobject/swfobject.js
+Request Method:GET
+Status Code:304 Not Modified
+Remote Address:103.28.22.108:80
+Response Headers
+view source
+Accept-Ranges:bytes
+Age:0
+Connection:Keep-Alive
+Content-Type:application/javascript
+Date:Tue, 08 Mar 2016 21:15:53 GMT
+ETag:"3aa08f-261f-50141c52b3b00"
+Last-Modified:Sat, 23 Aug 2014 01:21:16 GMT
+Server:Apache/2.2.24 (Unix) mod_ssl/2.2.24 OpenSSL/0.9.8e-fips-rhel5 mod_auth_passthrough/2.1 mod_bwlimited/1.4
+Request Headers
+view source
+Accept:*/*
+Accept-Encoding:gzip, deflate, sdch
+Accept-Language:en-US,en;q=0.8,id;q=0.6
+Cache-Control:max-age=0
+Connection:keep-alive
+Cookie:__utma=1.2011967162.1456677846.1457210241.1457282442.4; __utmz=1.1456677846.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=1.|2=product=vbates=1^3=channel=clkmidex=1^5=version=2.0.0.480=1; orGEO=true; orUserGeo=ID; orPing=true; __utma=131372565.1576031362.1456676207.1457367209.1457453644.8; __utmc=131372565; __utmz=131372565.1456676207.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); orPrepop=true
+Host:kedungrejo.com
+If-Modified-Since:Sat, 23 Aug 2014 01:21:16 GMT
+If-None-Match:"3aa08f-261f-50141c52b3b00"
+User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+Name
+fbMyApp.html
+swfobject.js
+2 requests ❘ 353 B transferred ❘ Finish: 354 ms ❘ DOMContentLoaded: 639 ms ❘ Load: 638 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-09 03:18:23.424 Navigated to file:///C:/Users/Axioo/Favorites/fbMyApp.html
+.
