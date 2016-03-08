0b96ecb140385b3fb071e33d3ab5667a0703f840
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-09 02:27:44 +0700

    Create skypeCheck.js

---
+++ http/download.skype.com/share/skypebuttons/js/skypeCheck.js
@@ -0,0 +1,48 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+http://download.skype.com/share/skypebuttons/js/skypeCheck.js
+-To be evaluated in console:
+Request URL:https://api.skype.com/detection/detection_as3.swf
+Request Method:GET
+Status Code:404 Not Found
+Remote Address:91.190.217.143:443
+Response Headers
+view source
+Connection:keep-alive
+Content-Length:591
+Content-Type:text/html
+Date:Tue, 08 Mar 2016 19:24:10 GMT
+ETag:"55ae4e34-24f"
+Server:nginx/1.7.8
+Strict-Transport-Security:max-age=7776000
+X-Content-Type-Options:nosniff
+X-Content-Type-Options:nosniff
+X-XSS-Protection:1; mode=block
+Request Headers
+view source
+Accept:*/*
+Accept-Encoding:gzip, deflate, sdch
+Accept-Language:en-US,en;q=0.8,id;q=0.6
+Connection:keep-alive
+Host:api.skype.com
+User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+X-Requested-With:ShockwaveFlash/20.0.0.306
+Name
+fbMyApp.html
+skypeCheck.js
+detection_as3.swf
+detection_as3.swf
+4 requests ❘ 1.1 KB transferred ❘ Finish: 2.05 s ❘ DOMContentLoaded: 1.03 s ❘ Load: 1.03 s
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-09 02:23:36.423 https://api.skype.com/detection/detection_as3.swf Failed to load resource: the server responded with a status of 404 (Not Found)
+2016-03-09 02:24:15.004 Navigated to file:///C:/Users/Axioo/Favorites/fbMyApp.html
+2016-03-09 02:24:16.950 detection_as3.swf:1 GET https://api.skype.com/detection/detection_as3.swf 404 (Not Found)
+.
