5c1892a40d35b4884bb341ea71c51756a80b673b
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-09 00:46:42 +0700

    Create r.js

---
+++ http/cdncache-a.akamaihd.net/js/d6f636e2f6a6562776e6574656b6/r.js
@@ -0,0 +1,51 @@
+-By Muhammad Adi Nugroho, S. Adm. Neg.:
+-Via My Notepad (HTML Editor):
+http://cdncache-a.akamaihd.net/js/d6f636e2f6a6562776e6574656b6/r.js
+-To be evaluated in console:
+Request URL:http://cdncache-a.akamaihd.net/js/d6f636e2f6a6562776e6574656b6/r.js
+Request Method:GET
+Status Code:304 Not Modified
+Remote Address:118.97.159.17:80
+Response Headers
+view source
+Cache-Control:max-age=8981
+Connection:keep-alive
+Content-Type:application/javascript
+Date:Tue, 08 Mar 2016 17:43:46 GMT
+Last-Modified:Tue, 08 Mar 2016 16:14:17 GMT
+Request Headers
+view source
+Accept:*/*
+Accept-Encoding:gzip, deflate, sdch
+Accept-Language:en-US,en;q=0.8,id;q=0.6
+Cache-Control:max-age=0
+Connection:keep-alive
+Host:cdncache-a.akamaihd.net
+If-Modified-Since:Tue, 08 Mar 2016 16:14:17 GMT
+User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+Name
+fbMyApp.html
+r.js
+2 requests ❘ 203 B transferred ❘ Finish: 411 ms ❘ DOMContentLoaded: 542 ms ❘ Load: 541 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-09 00:43:34.551 r.js:1 Uncaught ReferenceError: _GPL is not defined
+2016-03-09 00:43:52.560 Navigated to file:///C:/Users/Axioo/Favorites/fbMyApp.html
+2016-03-09 00:43:52.859
+r.js:1 Uncaught ReferenceError: _GPL is not defined
+(anonymous function)	@	r.js:1
+2016-03-09 00:44:09.925 Error saving setting with name: previouslyViewedFiles, value length: 3569. Error: Failed to set the 'previouslyViewedFiles' property on 'Storage': Setting the value of 'previouslyViewedFiles' exceeded the quota.
+2016-03-09 00:44:09.928 Ten largest settings: 
+2016-03-09 00:44:10.119 Setting: 'consoleHistory', size: 5235172
+2016-03-09 00:44:10.120 Setting: 'savedURLs', size: 3672
+2016-03-09 00:44:10.120 Setting: 'previouslyViewedFiles', size: 3530
+2016-03-09 00:44:10.120 Setting: 'breakpoints', size: 227
+2016-03-09 00:44:10.120 Setting: 'watchExpressions', size: 45
+.
