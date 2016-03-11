a773bf4146ad6e81e0096ac6ac8af1f2af70d64c
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-12 02:12:09 +0700

    Create swfobject.js

---
+++ Sources-http/kedungrejo.com/js/piecemaker/swfobject/swfobject.js
@@ -0,0 +1,29 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg.:
+-Sources:
+http://kedungrejo.com/js/piecemaker/swfobject/swfobject.js
+-To be evaluated in console:
+Request URL:file:///C:/Users/Axioo/Favorites/wallet.js
+Request Headers
+Provisional headers are shown
+Accept:*/*
+User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+Name
+tpc.html
+wallet.js
+UsersAxiooFavoriteswallet.js');
+3 requests ❘ 0 B transferred ❘ Finish: 99 ms ❘ DOMContentLoaded: 99 ms ❘ Load: 99 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-12 02:06:10.772 file:///C:/UsersAxiooFavoriteswallet.js'); Failed to load resource: net::ERR_FILE_NOT_FOUND
+2016-03-12 02:06:29.676 Navigated to file:///C:/Users/Axioo/Favorites/tpc.html
+2016-03-12 02:06:29.709
+tpc.html:14 GET file:///C:/UsersAxiooFavoriteswallet.js'); net::ERR_FILE_NOT_FOUND
+(anonymous function)	@	tpc.html:14
+.
