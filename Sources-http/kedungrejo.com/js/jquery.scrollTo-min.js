c64fa38ae1b3ad06f54c0d59cb14dc8bd238eceb
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-14 00:26:26 +0700

    Create jquery.scrollTo-min.js

---
+++ Sources-http/kedungrejo.com/js/jquery.scrollTo-min.js
@@ -0,0 +1,50 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Sources:
+http://kedungrejo.com/js/jquery.scrollTo-min.js
+-To be evaluated in console:
+Request URL:http://kedungrejo.com/js/jquery.scrollTo-min.js
+Request Method:GET
+Status Code:304 Not Modified
+Remote Address:103.28.22.108:80
+Response Headers
+view source
+Accept-Ranges:bytes
+Age:0
+Connection:Keep-Alive
+Content-Type:application/javascript
+Date:Sun, 13 Mar 2016 18:17:41 GMT
+ETag:"3980db-8d6-50141c0297e00"
+Last-Modified:Sat, 23 Aug 2014 01:19:52 GMT
+Server:Apache/2.2.24 (Unix) mod_ssl/2.2.24 OpenSSL/0.9.8e-fips-rhel5 mod_auth_passthrough/2.1 mod_bwlimited/1.4 FrontPage/5.0.2.2635
+Request Headers
+view source
+Accept:*/*
+Accept-Encoding:gzip, deflate, sdch
+Accept-Language:en-US,en;q=0.8,id;q=0.6
+Cache-Control:max-age=0
+Connection:keep-alive
+Cookie:__utma=1.2011967162.1456677846.1457210241.1457282442.4; __utmz=1.1456677846.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=1.|2=product=vbates=1^3=channel=clkmidex=1^5=version=2.0.0.480=1; orUserGeo=ID; orPrepop=true; __utmt=1; __utma=131372565.1576031362.1456676207.1457811858.1457889264.18; __utmb=131372565.2.10.1457889264; __utmc=131372565; __utmz=131372565.1456676207.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)
+Host:kedungrejo.com
+If-Modified-Since:Sat, 23 Aug 2014 01:19:52 GMT
+If-None-Match:"3980db-8d6-50141c0297e00"
+User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+Name
+tpc.html
+jquery.scrollTo-min.js
+2 requests ❘ 373 B transferred ❘ Finish: 180 ms ❘ DOMContentLoaded: 190 ms ❘ Load: 190 ms
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-14 00:22:27.595 jquery.scrollTo-min.js:11 Uncaught ReferenceError: jQuery is not defined
+2016-03-14 00:22:52.077
+jquery.scrollTo-min.js:11 Uncaught ReferenceError: jQuery is not defined
+(anonymous function)	@	jquery.scrollTo-min.js:11
+2016-03-14 00:22:52.151 Navigated to file:///C:/Users/Axioo/Favorites/tpc.html
+.
