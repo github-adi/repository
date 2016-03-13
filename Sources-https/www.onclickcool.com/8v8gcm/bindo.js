e9b1e071fe1250d4a34eb8cd121f4af0f60e9afa
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-14 00:57:53 +0700

    Create bindo.js

---
+++ Sources-https/www.onclickcool.com/8v8gcm/bindo.js
@@ -0,0 +1,42 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Sources:
+https://www.onclickcool.com/8v8gcm/bindo.js
+-To be evaluated in console:
+Request URL:https://www.onclickcool.com/8v8gcm/bindo.js
+Request Method:GET
+Status Code:304 Not Modified
+Remote Address:67.205.28.88:443
+Response Headers
+view source
+Connection:Keep-Alive
+Date:Sun, 13 Mar 2016 17:51:02 GMT
+ETag:"1b852-529a6ec0ae440"
+Keep-Alive:timeout=2, max=100
+Server:Apache
+Request Headers
+view source
+Accept:*/*
+Accept-Encoding:gzip, deflate, sdch
+Accept-Language:en-US,en;q=0.8,id;q=0.6
+Cache-Control:max-age=0
+Connection:keep-alive
+Host:www.onclickcool.com
+If-Modified-Since:Tue, 19 Jan 2016 02:49:45 GMT
+If-None-Match:"1b852-529a6ec0ae440"
+User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+Name
+tpc.html
+bindo.js
+2 requests ❘ 167 B transferred ❘ Finish: 5.65 s ❘ DOMContentLoaded: 5.82 s ❘ Load: 5.82 s
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-14 00:53:57.714 Navigated to file:///C:/Users/Axioo/Favorites/tpc.html
+.
