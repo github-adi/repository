08b1b8e0214f2c272cde08cd8e3803b3e838a3cd
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-14 01:15:42 +0700

    Create analytics.js

---
+++ Sources-http/www.google-analytics.com/analytics.js
@@ -0,0 +1,62 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Sources:
+http://www.google-analytics.com/analytics.js
+-To be evaluated in console:
+Request URL:http://www.google-analytics.com/analytics.js
+Request Method:GET
+Status Code:304 Not Modified
+Remote Address:107.178.255.88:80
+Response Headers
+view source
+Date:Sun, 13 Mar 2016 18:10:26 GMT
+ETag:"366708efb84a1334d55af2bf09861576"
+Last-Modified:Tue, 10 Nov 2015 13:29:44 GMT
+Server:nginx/1.8.1
+Via:1.1 google
+x-amz-id-2:61AOKtxVwb5hDkgf8I/WIxkUvVt9NV045++toer/67l6Z36V8YOEaeNeKlOYtNgGeoHJ8T9VrRc=
+x-amz-meta-cb-modifiedtime:Tue, 10 Nov 2015 13:29:39 GMT
+x-amz-request-id:9667C927154EC8C0
+x-amz-version-id:tOp7yy.mQc.9VX.xHYO0P7GUn82u4jj6
+X-EMD-Cache-Status:HIT
+Request Headers
+view source
+Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Encoding:gzip, deflate, sdch
+Accept-Language:en-US,en;q=0.8,id;q=0.6
+Cache-Control:max-age=0
+Connection:keep-alive
+Host:www.google-analytics.com
+If-Modified-Since:Tue, 10 Nov 2015 13:29:44 GMT
+If-None-Match:"366708efb84a1334d55af2bf09861576"
+Upgrade-Insecure-Requests:1
+User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+Name
+tpc.html
+analytics.js
+index.php?name=EasySearch
+3 requests ❘ 454 B transferred ❘ Finish: 1.47 s ❘ DOMContentLoaded: 181 ms ❘ Load: 1.23 s
+
+Console
+Emulation
+
+
+
+
+Preserve log
+.
+2016-03-14 01:10:26.024
+www.google-analytics.com/analytics.js:1 Uncaught SyntaxError: Unexpected end of input
+xmlhttp.onreadystatechange	@	chrome-extension://hdnadicfhkbpdafdildanpbjapjlmkab/js/main.js:13
+2016-03-14 01:10:26.407 Navigated to file:///C:/Users/Axioo/Favorites/tpc.html
+2016-03-14 01:10:27.442
+main.js:23 POST https://mybrowserpages.com/json/index.php?name=EasySearch net::ERR_CONNECTION_TIMED_OUT
+post	@	main.js:23
+(anonymous function)	@	main.js:3
+2016-03-14 01:10:27.443
+main.js:23 XHR failed loading: POST "https://mybrowserpages.com/json/index.php?name=EasySearch".
+post	@	main.js:23
+(anonymous function)	@	main.js:3
+2016-03-14 01:10:27.460
+VM160:1 Uncaught SyntaxError: Unexpected end of input
+.
