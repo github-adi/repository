d5ddc502b4f0ea49d84e5f1e6b074d03bb5efed9
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-15 04:03:54 +0700

    Create 41k8n.js

---
+++ Sources-http/cds.v3x3b3b5.hwcdn.net/k/602180/d1/41k8n.js
@@ -0,0 +1,151 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Sources:
+http://cds.v3x3b3b5.hwcdn.net/k/602180/d1/41k8n.
+-HAR:
+{
+  "log": {
+    "version": "1.2",
+    "creator": {
+      "name": "WebInspector",
+      "version": "537.36"
+    },
+    "pages": [
+      {
+        "startedDateTime": "2016-03-14T21:03:03.879Z",
+        "id": "page_1",
+        "title": "file:///C:/Users/Axioo/Favorites/tpc.html",
+        "pageTimings": {
+          "onContentLoad": 864.0889999987849,
+          "onLoad": 863.3479999989504
+        }
+      }
+    ],
+    "entries": [
+      {
+        "startedDateTime": "2016-03-14T21:03:04.053Z",
+        "time": 582.1480000013253,
+        "request": {
+          "method": "GET",
+          "url": "http://cds.v3x3b3b5.hwcdn.net/k/602180/d1/41k8n.js",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, sdch"
+            },
+            {
+              "name": "Host",
+              "value": "cds.v3x3b3b5.hwcdn.net"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "en-US,en;q=0.8,id;q=0.6"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"
+            },
+            {
+              "name": "Accept",
+              "value": "*/*"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=0"
+            },
+            {
+              "name": "If-None-Match",
+              "value": "\"1457379812\""
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "If-Modified-Since",
+              "value": "Mon, 07 Mar 2016 19:43:32 GMT"
+            }
+          ],
+          "queryString": [],
+          "cookies": [],
+          "headersSize": 406,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 200,
+          "statusText": "OK",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Date",
+              "value": "Mon, 14 Mar 2016 21:03:04 GMT"
+            },
+            {
+              "name": "Last-Modified",
+              "value": "Mon, 07 Mar 2016 16:58:22 GMT"
+            },
+            {
+              "name": "Age",
+              "value": "0"
+            },
+            {
+              "name": "ETag",
+              "value": "\"1457369902\""
+            },
+            {
+              "name": "X-HW",
+              "value": "1457932184.dop001.si1.t,1457932183.cds008.si1.c"
+            },
+            {
+              "name": "Content-Type",
+              "value": "text/javascript"
+            },
+            {
+              "name": "Access-Control-Allow-Origin",
+              "value": "*"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=172800"
+            },
+            {
+              "name": "Connection",
+              "value": "Keep-Alive"
+            },
+            {
+              "name": "Accept-Ranges",
+              "value": "bytes"
+            },
+            {
+              "name": "Content-Length",
+              "value": "51553"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 51553,
+            "mimeType": "text/javascript",
+            "compression": 0
+          },
+          "redirectURL": "",
+          "headersSize": 348,
+          "bodySize": 51553,
+          "_transferSize": 51901
+        },
+        "cache": {},
+        "timings": {
+          "blocked": 1.79500000012922,
+          "dns": 142.04600000084577,
+          "connect": 40.722000001552004,
+          "send": 1.2469999965099987,
+          "wait": 135.781000000862,
+          "receive": 260.5570000014263,
+          "ssl": -1
+        },
+        "connection": "34966",
+        "pageref": "page_1"
+      }
+    ]
+  }
+}
