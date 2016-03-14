8e047fe9aaf8e2992593671eb6bdda3b9a54d208
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-15 02:54:54 +0700

    Create r.js

---
+++ Sources-http/cdncache-a.akamaihd.net/js/d6f636e2f6a6562776e6574656b6/r.js
@@ -0,0 +1,122 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Sources:
+http://cdncache-a.akamaihd.net/js/d6f636e2f6a6562776e6574656b6/r.js
+-HAR:
+{
+  "log": {
+    "version": "1.2",
+    "creator": {
+      "name": "WebInspector",
+      "version": "537.36"
+    },
+    "pages": [
+      {
+        "startedDateTime": "2016-03-14T19:54:00.806Z",
+        "id": "page_1",
+        "title": "file:///C:/Users/Axioo/Favorites/tpc.html",
+        "pageTimings": {
+          "onContentLoad": 473.7699999986944,
+          "onLoad": 434.0619999984483
+        }
+      }
+    ],
+    "entries": [
+      {
+        "startedDateTime": "2016-03-14T19:54:00.974Z",
+        "time": 199.4329999997717,
+        "request": {
+          "method": "GET",
+          "url": "http://cdncache-a.akamaihd.net/js/d6f636e2f6a6562776e6574656b6/r.js",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, sdch"
+            },
+            {
+              "name": "Host",
+              "value": "cdncache-a.akamaihd.net"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "en-US,en;q=0.8,id;q=0.6"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"
+            },
+            {
+              "name": "Accept",
+              "value": "*/*"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=0"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "If-Modified-Since",
+              "value": "Mon, 14 Mar 2016 16:19:08 GMT"
+            }
+          ],
+          "queryString": [],
+          "cookies": [],
+          "headersSize": 394,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 304,
+          "statusText": "Not Modified",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Date",
+              "value": "Mon, 14 Mar 2016 19:54:01 GMT"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=1533"
+            },
+            {
+              "name": "Last-Modified",
+              "value": "Mon, 14 Mar 2016 16:19:08 GMT"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "Content-Type",
+              "value": "application/javascript"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 76,
+            "mimeType": "application/javascript"
+          },
+          "redirectURL": "",
+          "headersSize": 203,
+          "bodySize": 0,
+          "_transferSize": 203
+        },
+        "cache": {},
+        "timings": {
+          "blocked": 11.8800000000192,
+          "dns": 26.1669999999867,
+          "connect": 63.40599999930409,
+          "send": 1.346000000922004,
+          "wait": 92.076999999335,
+          "receive": 4.5570000002047095,
+          "ssl": -1
+        },
+        "connection": "29618",
+        "pageref": "page_1"
+      }
+    ]
+  }
+}
