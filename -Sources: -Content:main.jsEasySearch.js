504d9a42fa0fe188ee1cb836f7eb3754a0f2156e
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-17 00:33:17 +0700

    Create -Sources: -Content:main.jsEasySearch.js

---
+++ -Sources: -Content:main.jsEasySearch.js
@@ -0,0 +1,245 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Sources:
+-Content:
+<html>
+ <head>
+	<meta charset="utf-8">
+	<script type="text/javascript" src="chrome-extension://hdnadicfhkbpdafdildanpbjapjlmkab/js/ga.js"></script>
+	<script type="text/javascript" src="chrome-extension://hdnadicfhkbpdafdildanpbjapjlmkab/js/background.js"></script>
+<script type="text/javascript" src="chrome-extension://hdnadicfhkbpdafdildanpbjapjlmkab/js/main.js"></script>
+<script type="text/javascript" src="chrome-extension://hdnadicfhkbpdafdildanpbjapjlmkab/js/EasySearch.js"></script>
+
+ </head>
+ <body>
+ </body>
+</html>
+-HAR:
+
+{
+  "log": {
+    "version": "1.2",
+    "creator": {
+      "name": "WebInspector",
+      "version": "537.36"
+    },
+    "pages": [
+      {
+        "startedDateTime": "2016-03-16T17:23:14.344Z",
+        "id": "page_1",
+        "title": "file:///C:/Users/Axioo/Favorites/tpc.html",
+        "pageTimings": {
+          "onContentLoad": 1916.5950000005978,
+          "onLoad": 1958.943999999974
+        }
+      }
+    ],
+    "entries": [
+      {
+        "startedDateTime": "2016-03-16T17:23:14.554Z",
+        "time": 1541.4489999993748,
+        "request": {
+          "method": "GET",
+          "url": "http://api.jollywallet.com/affiliate/client?p=16&dist=261&sub=chrome208",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, sdch"
+            },
+            {
+              "name": "Host",
+              "value": "api.jollywallet.com"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "en-US,en;q=0.8,id;q=0.6"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"
+            },
+            {
+              "name": "Accept",
+              "value": "*/*"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=0"
+            },
+            {
+              "name": "Cookie",
+              "value": "jw_ab=ALL; jw_msg3_a=0; __utma=153538851.944972817.1451402028.1457702799.1458056595.24; __utmz=153538851.1458056595.24.24.utmcsr=flirchi.com|utmccn=(referral)|utmcmd=referral|utmcct=/; jw_class=1; jw_token=%22EC4386CA-EBA2-08F2-8B20-0776B4598AF8%22; jw_platform=16; jw_session_EC4386CA-EBA2-08F2-8B20-0776B4598AF8=%5B%221050288785%22%2C%22117%22%2C%225%22%2C0%2C%220%22%2C%2213%3A12%3A29+26%5C%2F12%5C%2F15%22%2C%2212%3A10%3A54+16%5C%2F03%5C%2F16%22%2C%220%22%2C%220%22%2C%220%22%2C%220%22%2C%22%22%2C%22%22%2C%2210000000000%22%2C%22117%22%2C%22Chrome%22%2C%2247.0.2526.106%22%2C%220%22%2C%220%22%2C%221%22%2C%22261%22%2C%22chrome208%22%2C%220%22%2C%7B%222%22%3A%7B%22last_display_date%22%3A1458056594%2C%22display_count%22%3A1%2C%22automatic_closed%22%3A1%2C%22closed_by_user%22%3A1%2C%22eliminate%22%3Afalse%2C%22sub_session%22%3A0%7D%7D%2C%2275%22%2C%221%22%2C%221%22%2C%221%22%2C%221451154124%22%2C%223%22%2C%220%22%2C%220%22%2C%22ID%22%5D"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            }
+          ],
+          "queryString": [
+            {
+              "name": "p",
+              "value": "16"
+            },
+            {
+              "name": "dist",
+              "value": "261"
+            },
+            {
+              "name": "sub",
+              "value": "chrome208"
+            }
+          ],
+          "cookies": [
+            {
+              "name": "jw_ab",
+              "value": "ALL",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "jw_msg3_a",
+              "value": "0",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utma",
+              "value": "153538851.944972817.1451402028.1457702799.1458056595.24",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utmz",
+              "value": "153538851.1458056595.24.24.utmcsr=flirchi.com|utmccn=(referral)|utmcmd=referral|utmcct=/",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "jw_class",
+              "value": "1",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "jw_token",
+              "value": "%22EC4386CA-EBA2-08F2-8B20-0776B4598AF8%22",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "jw_platform",
+              "value": "16",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "jw_session_EC4386CA-EBA2-08F2-8B20-0776B4598AF8",
+              "value": "%5B%221050288785%22%2C%22117%22%2C%225%22%2C0%2C%220%22%2C%2213%3A12%3A29+26%5C%2F12%5C%2F15%22%2C%2212%3A10%3A54+16%5C%2F03%5C%2F16%22%2C%220%22%2C%220%22%2C%220%22%2C%220%22%2C%22%22%2C%22%22%2C%2210000000000%22%2C%22117%22%2C%22Chrome%22%2C%2247.0.2526.106%22%2C%220%22%2C%220%22%2C%221%22%2C%22261%22%2C%22chrome208%22%2C%220%22%2C%7B%222%22%3A%7B%22last_display_date%22%3A1458056594%2C%22display_count%22%3A1%2C%22automatic_closed%22%3A1%2C%22closed_by_user%22%3A1%2C%22eliminate%22%3Afalse%2C%22sub_session%22%3A0%7D%7D%2C%2275%22%2C%221%22%2C%221%22%2C%221%22%2C%221451154124%22%2C%223%22%2C%220%22%2C%220%22%2C%22ID%22%5D",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            }
+          ],
+          "headersSize": 1300,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 200,
+          "statusText": "OK",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Date",
+              "value": "Wed, 16 Mar 2016 17:23:14 GMT"
+            },
+            {
+              "name": "Server",
+              "value": "nginx/1.4.7"
+            },
+            {
+              "name": "P3P",
+              "value": "policyref=\"/w3c/p3p.xml\", CP=\"CURa ADMa DEVa OUR IND DSP CAO COR\""
+            },
+            {
+              "name": "X-Powered-By",
+              "value": "PHP/5.4.38"
+            },
+            {
+              "name": "Content-Type",
+              "value": "application/javascript"
+            },
+            {
+              "name": "Set-Cookie",
+              "value": "jw_platform=16; expires=Wed, 23-Mar-2016 17:23:14 GMT; path=/; domain=jollywallet.com"
+            },
+            {
+              "name": "Set-Cookie",
+              "value": "jw_token=%22EC4386CA-EBA2-08F2-8B20-0776B4598AF8%22; expires=Fri, 24-Jun-2016 17:23:14 GMT; path=/; domain=jollywallet.com"
+            },
+            {
+              "name": "Connection",
+              "value": "Keep-Alive"
+            },
+            {
+              "name": "Content-Length",
+              "value": "42783"
+            },
+            {
+              "name": "Expires",
+              "value": "Sun, 20 Mar 2016 17:23:14 GMT"
+            }
+          ],
+          "cookies": [
+            {
+              "name": "jw_platform",
+              "value": "16",
+              "path": "/",
+              "domain": "jollywallet.com",
+              "expires": "2016-03-23T17:23:14.000Z",
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "jw_token",
+              "value": "%22EC4386CA-EBA2-08F2-8B20-0776B4598AF8%22",
+              "path": "/",
+              "domain": "jollywallet.com",
+              "expires": "2016-06-24T17:23:14.000Z",
+              "httpOnly": false,
+              "secure": false
+            }
+          ],
+          "content": {
+            "size": 42783,
+            "mimeType": "application/javascript",
+            "compression": 0
+          },
+          "redirectURL": "",
+          "headersSize": 535,
+          "bodySize": 42783,
+          "_transferSize": 43318
+        },
+        "cache": {},
+        "timings": {
+          "blocked": 2.15399999979127,
+          "dns": 48.84299999957873,
+          "connect": 40.0180000006003,
+          "send": 2.5509999995848034,
+          "wait": 627.2570000001,
+          "receive": 820.6259999997197,
+          "ssl": -1
+        },
+        "connection": "56410",
+        "pageref": "page_1"
+      }
+    ]
+  }
+}
