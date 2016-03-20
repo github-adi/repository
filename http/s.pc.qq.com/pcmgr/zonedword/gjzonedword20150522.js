a28a124e744510673aa044811b8dbc870fe84b93
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-21 00:52:49 +0700

    Create gjzonedword20150522.js

---
+++ http/s.pc.qq.com/pcmgr/zonedword/gjzonedword20150522.js
@@ -0,0 +1,436 @@
+-By Muhammad Adi Nugroho,S.Adm.Neg:
+-Via My Notepad (HTML Editor):
+http://s.pc.qq.com/pcmgr/zonedword/gjzonedword20150522.js
+-HAR:
+{
+  "log": {
+    "version": "1.2",
+    "creator": {
+      "name": "WebInspector",
+      "version": "537.36"
+    },
+    "pages": [
+      {
+        "startedDateTime": "2016-03-20T17:37:21.299Z",
+        "id": "page_1",
+        "title": "file:///C:/Users/Axioo/Favorites/tpc.html",
+        "pageTimings": {
+          "onContentLoad": 475.49999999955617,
+          "onLoad": 1799.9859999999899
+        }
+      }
+    ],
+    "entries": [
+      {
+        "startedDateTime": "2016-03-20T17:37:21.521Z",
+        "time": 203.5940000005212,
+        "request": {
+          "method": "GET",
+          "url": "http://s.pc.qq.com/pcmgr/zonedword/gjzonedword20150522.js",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, sdch"
+            },
+            {
+              "name": "Host",
+              "value": "s.pc.qq.com"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "en-US,en;q=0.8,id;q=0.6"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"
+            },
+            {
+              "name": "Accept",
+              "value": "*/*"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=0"
+            },
+            {
+              "name": "Cookie",
+              "value": "__utma=202488848.904670889.1449948938.1449948938.1449948938.1; __utmz=202488848.1449948938.1.1.utmcsr=onlinehtmleditor.net|utmccn=(referral)|utmcmd=referral|utmcct=/; __utma=136017777.1903280872.1449948943.1455380527.1455450765.3; __utmz=136017777.1455450765.3.3.utmcsr=s.pc.qq.com|utmccn=(referral)|utmcmd=referral|utmcct=/pcmgr/index.html; pgv_pvid=1336665078; pgv_pvi=9448780800; pgv_info=ssi=s8369746618"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "If-Modified-Since",
+              "value": "Tue, 19 Jan 2016 03:36:08 GMT"
+            }
+          ],
+          "queryString": [],
+          "cookies": [
+            {
+              "name": "__utma",
+              "value": "202488848.904670889.1449948938.1449948938.1449948938.1",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utmz",
+              "value": "202488848.1449948938.1.1.utmcsr=onlinehtmleditor.net|utmccn=(referral)|utmcmd=referral|utmcct=/",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utma",
+              "value": "136017777.1903280872.1449948943.1455380527.1455450765.3",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utmz",
+              "value": "136017777.1455450765.3.3.utmcsr=s.pc.qq.com|utmccn=(referral)|utmcmd=referral|utmcct=/pcmgr/index.html",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "pgv_pvid",
+              "value": "1336665078",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "pgv_pvi",
+              "value": "9448780800",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "pgv_info",
+              "value": "ssi=s8369746618",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            }
+          ],
+          "headersSize": 800,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 304,
+          "statusText": "Not Modified",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Date",
+              "value": "Sun, 20 Mar 2016 17:37:20 GMT"
+            },
+            {
+              "name": "X-Cache-Lookup",
+              "value": "Hit From Disktank Gz"
+            },
+            {
+              "name": "Last-Modified",
+              "value": "Tue, 19 Jan 2016 03:36:08 GMT"
+            },
+            {
+              "name": "Server",
+              "value": "X2_Platform"
+            },
+            {
+              "name": "Age",
+              "value": "0"
+            },
+            {
+              "name": "Content-Type",
+              "value": "application/x-javascript"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=604800"
+            },
+            {
+              "name": "Connection",
+              "value": "Keep-Alive"
+            },
+            {
+              "name": "Expires",
+              "value": "Sun, 27 Mar 2016 17:37:20 GMT"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 9541,
+            "mimeType": "application/x-javascript"
+          },
+          "redirectURL": "",
+          "headersSize": 314,
+          "bodySize": 0,
+          "_transferSize": 314
+        },
+        "cache": {},
+        "timings": {
+          "blocked": 28.4520000004704,
+          "dns": -1,
+          "connect": -1,
+          "send": 1.6889999997148024,
+          "wait": 172.1699999998238,
+          "receive": 1.283000000512203,
+          "ssl": -1
+        },
+        "connection": "15013",
+        "pageref": "page_1"
+      },
+      {
+        "startedDateTime": "2016-03-20T17:37:21.762Z",
+        "time": 173.3329999997295,
+        "request": {
+          "method": "GET",
+          "url": "http://s.pc.qq.com/pcmgr/zonedword/gjzonedword20150522.js",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, sdch"
+            },
+            {
+              "name": "Host",
+              "value": "s.pc.qq.com"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "en-US,en;q=0.8,id;q=0.6"
+            },
+            {
+              "name": "Upgrade-Insecure-Requests",
+              "value": "1"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"
+            },
+            {
+              "name": "Accept",
+              "value": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=0"
+            },
+            {
+              "name": "Cookie",
+              "value": "__utma=202488848.904670889.1449948938.1449948938.1449948938.1; __utmz=202488848.1449948938.1.1.utmcsr=onlinehtmleditor.net|utmccn=(referral)|utmcmd=referral|utmcct=/; __utma=136017777.1903280872.1449948943.1455380527.1455450765.3; __utmz=136017777.1455450765.3.3.utmcsr=s.pc.qq.com|utmccn=(referral)|utmcmd=referral|utmcct=/pcmgr/index.html; pgv_pvid=1336665078; pgv_pvi=9448780800; pgv_info=ssi=s8369746618"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "If-Modified-Since",
+              "value": "Tue, 19 Jan 2016 03:36:08 GMT"
+            }
+          ],
+          "queryString": [],
+          "cookies": [
+            {
+              "name": "__utma",
+              "value": "202488848.904670889.1449948938.1449948938.1449948938.1",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utmz",
+              "value": "202488848.1449948938.1.1.utmcsr=onlinehtmleditor.net|utmccn=(referral)|utmcmd=referral|utmcct=/",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utma",
+              "value": "136017777.1903280872.1449948943.1455380527.1455450765.3",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "__utmz",
+              "value": "136017777.1455450765.3.3.utmcsr=s.pc.qq.com|utmccn=(referral)|utmcmd=referral|utmcct=/pcmgr/index.html",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "pgv_pvid",
+              "value": "1336665078",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "pgv_pvi",
+              "value": "9448780800",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            },
+            {
+              "name": "pgv_info",
+              "value": "ssi=s8369746618",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            }
+          ],
+          "headersSize": 901,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 304,
+          "statusText": "Not Modified",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Date",
+              "value": "Sun, 20 Mar 2016 17:37:20 GMT"
+            },
+            {
+              "name": "X-Cache-Lookup",
+              "value": "Hit From Disktank Gz"
+            },
+            {
+              "name": "Last-Modified",
+              "value": "Tue, 19 Jan 2016 03:36:08 GMT"
+            },
+            {
+              "name": "Server",
+              "value": "X2_Platform"
+            },
+            {
+              "name": "Age",
+              "value": "0"
+            },
+            {
+              "name": "Content-Type",
+              "value": "application/x-javascript"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=604800"
+            },
+            {
+              "name": "Connection",
+              "value": "Keep-Alive"
+            },
+            {
+              "name": "Expires",
+              "value": "Sun, 27 Mar 2016 17:37:20 GMT"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 9541,
+            "mimeType": "application/x-javascript"
+          },
+          "redirectURL": "",
+          "headersSize": 314,
+          "bodySize": 0,
+          "_transferSize": 314
+        },
+        "cache": {},
+        "timings": {
+          "blocked": 5.40900000032707,
+          "dns": -1,
+          "connect": -1,
+          "send": 1.8579999996291,
+          "wait": 164.87599999982183,
+          "receive": 1.1899999999515103,
+          "ssl": -1
+        },
+        "connection": "15013",
+        "pageref": "page_1"
+      },
+      {
+        "startedDateTime": "2016-03-20T17:37:22.089Z",
+        "time": 0,
+        "request": {
+          "method": "POST",
+          "url": "https://mybrowserpages.com/json/index.php?name=EasySearch",
+          "httpVersion": "unknown",
+          "headers": [
+            {
+              "name": "Origin",
+              "value": "http://s.pc.qq.com"
+            },
+            {
+              "name": "Referer",
+              "value": "http://s.pc.qq.com/pcmgr/zonedword/gjzonedword20150522.js"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36"
+            }
+          ],
+          "queryString": [
+            {
+              "name": "name",
+              "value": "EasySearch"
+            }
+          ],
+          "cookies": [],
+          "headersSize": -1,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 0,
+          "statusText": "",
+          "httpVersion": "unknown",
+          "headers": [],
+          "cookies": [],
+          "content": {
+            "size": 0,
+            "mimeType": "x-unknown"
+          },
+          "redirectURL": "",
+          "headersSize": -1,
+          "bodySize": -1,
+          "_transferSize": 0,
+          "_error": "net::ERR_CONNECTION_TIMED_OUT"
+        },
+        "cache": {},
+        "timings": {
+          "blocked": -1,
+          "dns": -1,
+          "connect": -1,
+          "send": 0,
+          "wait": 0,
+          "receive": 0,
+          "ssl": -1
+        },
+        "pageref": "page_1"
+      }
+    ]
+  }
+}
+-To be evaluated in console:
+2016-03-21 00:36:44.348 c:UsersAxiooFavoritesad.js'); Failed to load resource: net::ERR_FILE_NOT_FOUND
+2016-03-21 00:37:04.958 https://mybrowserpages.com/json/index.php?name=EasySearch Failed to load resource: net::ERR_CONNECTION_TIMED_OUT
+2016-03-21 00:37:04.975 gjzonedword20150522.js:1 Uncaught SyntaxError: Unexpected end of input
+2016-03-21 00:37:21.562 Navigated to file:///C:/Users/Axioo/Favorites/tpc.html
+2016-03-21 00:37:21.798 tpc.html:13 GET c:UsersAxiooFavoritesad.js'); net::ERR_FILE_NOT_FOUND(anonymous function) @ tpc.html:13
+2016-03-21 00:37:42.758 main.js:23 POST https://mybrowserpages.com/json/index.php?name=EasySearch net::ERR_CONNECTION_TIMED_OUTpost @ main.js:23(anonymous function) @ main.js:3
+2016-03-21 00:37:42.758 main.js:23 XHR failed loading: POST "https://mybrowserpages.com/json/index.php?name=EasySearch".post @ main.js:23(anonymous function) @ main.js:3
+2016-03-21 00:37:42.771 VM160:1 Uncaught SyntaxError: Unexpected end of inputxmlhttp.onreadystatechange @ main.js:13XMLHttpRequest.send (async)post @ main.js:23(anonymous function) @ main.js:3
+Name	
+tpc.html	
+gjzonedword20150522.js	
+gjzonedword20150522.js	
+c:UsersAxiooFavoritesad.js');	
+index.php?name=EasySearch	
+5 requests ❘ 628 B transferred ❘ Finish: 21.46 s ❘ DOMContentLoaded: 475 ms ❘ Load: 1.80 s
+
