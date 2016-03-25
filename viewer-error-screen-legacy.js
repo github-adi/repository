0f7ce76e88f22e551aa87c5122ecd4cef6e08784
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-03-26 01:00:52 +0700

    Create viewer-error-screen-legacy.js

---
+++ viewer-error-screen-legacy.js
@@ -0,0 +1,16 @@
+-My build Developer Programming Languages:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+viewer-error-screen-legacy.js
+
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+Polymer({
+  is: 'viewer-error-screen-legacy',
+  properties: {
+    text: String
+  }
+
+  
+  
