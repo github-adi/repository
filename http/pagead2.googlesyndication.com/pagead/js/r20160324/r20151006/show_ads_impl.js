b25d5e91cc81314af01a1ae3eb22890e21126f4e
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-04-03 00:32:08 +0700

    Create show_ads_impl.js

---
+++ http/pagead2.googlesyndication.com/pagead/js/r20160324/r20151006/show_ads_impl.js
@@ -0,0 +1,17 @@
+-My build DIGITAL Command Language:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Returned f download:
+<body>
+<script src=" http://pagead2.googlesyndication.com/pagead/js/r20160324/r20151006/show_ads_impl.js "></script>
+<iframe src=" http://pagead2.googlesyndication.com/pagead/js/r20160324/r20151006/show_ads_impl.js "></iframe>
+<script> 
+ (adsbygoogle = window.adsbygoogle || []).push({ 
+google_ad_client: "ca-pub-5138331548304515", 
+enable_page_level_ads: true 
+});
+</script>
+
+</body>
+
+
