2bf5e55e114c65162cbad6025f4a76c461db2df5
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-04-20 02:26:27 +0700

    Create notifications-worker.jsanalytics.js

---
+++ notifications-worker.jsanalytics.js
@@ -0,0 +1,26 @@
+-My build DIGITAL Command Language:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+<body>
+	  <script src=" https://flirchi.com/notifications-worker.js "></script>
+	  <script src=" C:\Users\Axioo\Favorites\analytics.js "></script>
+	  <script>
+
+{
+var c=document.createElement("iframe");
+
+c.setAttribute("type","text/javascript");
+
+c.setAttribute("src"," https://flirchi.com/notifications-worker.js ");
+
+document.body.appendChild(c)
+}
+
+</script>
+	  	  
+</body>
+-Returned:
+-DevTools:Service Workers:Stopped:inspect:DevToolS https://flirchi.com/notifications-worker.js
+-HAR:
+
+-To be evaluated in console:
