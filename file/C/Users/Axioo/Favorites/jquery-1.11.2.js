ca0c1d78a58a6dd93ec893c9a62b6d0593a3ec22
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-05-30 23:55:16 +0700

    Create jquery-1.11.2.js

---
+++ file/C/Users/Axioo/Favorites/jquery-1.11.2.js
@@ -0,0 +1,18 @@
+-My build DIGITAL Command Language:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad (HTML Editor):
+-Run in Your Notepad:
+<body>
+
+<script type = "text/javascript" 
+         src = "  file:///C:/Users/Axioo/Favorites/jquery-1.11.2.js "></script>
+<script type = "text/javascript" 
+         src = "file:///C:/Users/Axioo/Favorites/telkom2&params.js
+
+		 "></script>
+
+</body>
+
+-HAR:
+
+-To be evaluated in console:
