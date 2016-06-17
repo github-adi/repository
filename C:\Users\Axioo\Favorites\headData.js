ce152c11849b0b3bae956a9c4536b4fa0fc7bb57
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-06-18 00:13:41 +0700

    Create C:\Users\Axioo\Favorites\headData.js

---
+++ C:\Users\Axioo\Favorites\headData.js
@@ -0,0 +1,70 @@
+-My build DIGITAL Command Language:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad++ (HTML Editor):
+<!DOCTYPE html><html prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"  lang="id-ID"  style="font-size:62.5%"  class="hiperf"  dir="ltr" >
+
+ <body>
+ 
+ <SCRIPT TYPE = "text/javascript" 
+         SRC = " C:\Users\Axioo\Favorites\headData.js "></SCRIPT>
+			
+	 
+		 </body>
+		 </html>
+		 		 
+-Function Identifier:
+asdfgo1312
+true
+-
+applicationCache
+OfflineResourceList { status: 0, onchecking: null, onerror: null, onnoupdate: null, ondownloading: null, onprogress: null, onupdateready: null, oncached: null, onobsolete: null, mozItems: DOMStringList[0] }
+-
+unreachable code after return statement client:380:1
+unreachable code after return statement client:380:1
+SyntaxError: expected expression, got '<' extension.js:1:0
+aff
+"mikes-top-picks.com"
+Prefixed Fullscreen API is deprecated. Please use unprefixed API for fullscreen. For more help https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API rhpop_1.1.45.js:1:2519
+'mozHidden' and 'mozVisibilityState' are deprecated.  Please use the unprefixed 'hidden' and 'visibilityState' instead. rhpop_1.1.45.js:1:2519
+-
+a8
+"navigator"
+-
+a
+"file:///C:/Users/Axioo/Favorites/pub.html"
+-
+a.__proto__
+String
+-
+crypto
+Crypto { subtle: SubtleCrypto }
+-
+content
+Window → file:///C:/Users/Axioo/Favorites/pub.html
+-
+closed
+false
+-
+Controllers
+DOMPrototype { , 1 more… }
+-
+domain_for_sanity
+undefined
+-
+document
+HTMLDocument → file:///C:/Users/Axioo/Favorites/pub.html
+23:56:21.808 HTMLDocument → file:///C:/Users/Axioo/Favorites/pub.html
+
+-
+dispached
+true
+-
+devicePixelRatio
+1
+-
+DealPlyDomParams
+Object { partner: "crbfii", channel: "crbfiicrdr_300303410029000000", sset: "1", dom: "", fq: "", itype: "i", countryCode: "ID", appTitle: "Deal 4 Us", scriptTagUrl: "//i.crbfiijs.info/crbfii/javascript…", firstTime: "false", 8 more… }
+
+
+
+
