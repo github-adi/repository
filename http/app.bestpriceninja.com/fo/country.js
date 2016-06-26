5348a51077facef0f3edee391a6bf35accc35c8a
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-06-27 03:06:46 +0700

    Create country.js

---
+++ http/app.bestpriceninja.com/fo/country.js
@@ -0,0 +1,11 @@
+-My build of DIGITAL Command Language:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad++ (HTML Editor):
+-Run at Developer tools Mozilla Firefox/Google Chrome: Preview/Response:
+-Returned Your Country Code:
+<body>
+<SCRIPT SRC=" http://app.bestpriceninja.com/fo/country.js "></SCRIPT>
+</body>
+-HAR:
+
+-To be evaluated in console:
