ba9ca0580b5f027aab3b548d3132da55056a204b
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-07-21 22:46:45 +0700

    Create 83c584abc496247d71ded7a18e996418.js

---
+++ https/gist.github.com/alph4delta/83c584abc496247d71ded7a18e996418.js
@@ -0,0 +1,24 @@
+-My build of DIGITAL Command Language:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad++ (HTML Editor):
+<body>
+ <html>
+ 
+ <SCRIPT TYPE = "TEXT/JAVASCRIPT" 
+         SRC = " https://gist.github.com/alph4delta/83c584abc496247d71ded7a18e996418.js "></SCRIPT>
+<script TYPE= "TEXT/JAVASCRIPT" src="https://gist.github.com/alph4delta/d65931d112e3529a2775b3e43809633d.js"></script>
+<script TYPE= "TEXT/JAVASCRIPT" src="https://gist.github.com/alph4delta/4e6a471d6ddd10f878704cc905da1c6b.js"></script>
+<script TYPE= "TEXT/JAVASCRIPT" src="https://gist.github.com/alph4delta/8907a20047139320e1be3c01aa875de0.js"></script>
+<script TYPE= "TEXT/JAVASCRIPT" src="https://gist.github.com/alph4delta/8a28371b87688d95bdf2f940bd4d3e54.js"></script>
+<script TYPE= "TEXT/JAVASCRIPT" src="https://gist.github.com/anonymous/30111e5036bd9faf959ea92e828fe8f9.js"></script>
+
+
+   </html>  
+   </body>   
+       
+-HAR:
+
+-To be evaluated in console:
+
+-Benchmark:
+-Developer Tools:
