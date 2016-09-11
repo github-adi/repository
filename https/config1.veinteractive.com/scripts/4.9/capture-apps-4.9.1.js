d85ac922d08dc4fdc4c90d113509d072d63083fb
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2016-09-12 01:06:10 +0700

    Create capture-apps-4.9.1.js

---
+++ https/config1.veinteractive.com/scripts/4.9/capture-apps-4.9.1.js
@@ -0,0 +1,7 @@
+-My build of DIGITAL Command Language:
+-Muhammad Adi Nugroho,S.Adm.Neg.:
+-Via My Notepad++ (HTML Editor):
+https://config1.veinteractive.com/scripts/4.9/capture-apps-4.9.1.js
+-Via Console Google Chrome:
+-Returned:
+true
