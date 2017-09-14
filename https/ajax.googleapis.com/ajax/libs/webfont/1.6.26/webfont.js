71f102439203a17740b8e5a3458c8e86da6cc88c
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2017-09-14 08:42:49 +0700

    Create webfont.js

---
+++ https/ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js
@@ -0,0 +1,9 @@
+//*Memanggil Libraries Google Fonts di DevTools*\\
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+<script>
+  WebFont.load({
+    google: {
+      families: ['Droid Sans', 'Droid Serif']
+    }
+  });
+</script>
